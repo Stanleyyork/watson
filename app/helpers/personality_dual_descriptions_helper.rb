f27d53289277dc891030e9b7908904dca7bbedb3
stanleyyork <stanleyyork@gmail.com>
2016-01-08 15:54:13 -0800

    added new tables for personality descriptions

---
+++ app/helpers/personality_dual_descriptions_helper.rb
@@ -0,0 +1,2 @@
+module PersonalityDualDescriptionsHelper
+end
---
+++ test/models/personality_dual_description_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PersonalityDualDescriptionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
