9c8a7bba53e7accf607568ac88e88f3432066a61
stanleyyork <stanleyyork@gmail.com>
2016-01-07 16:14:42 -0800

    init commit; including user model and sessions

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  resources :users
+end
