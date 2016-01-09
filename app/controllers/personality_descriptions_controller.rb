12ac3ca297aca36fe93b302d80a2c04ef49fd50e
stanleyyork <stanleyyork@gmail.com>
2016-01-08 16:36:57 -0800

    deleted unneeded files

--- app/controllers/personality_descriptions_controller.rb
+++ app/controllers/personality_descriptions_controller.rb
@@ -1,18 +1,2 @@
 class PersonalityDescriptionsController < ApplicationController
-  def new
-  	personality_description = PersonalityDescription.new
-  end
-
-  def create
-  	pd_params = params.require(:personality_descriptions).permit(:category, :attribute_name, :high_description)
-  	puts pd_params
-  	personality_description = PersonalityDescription.new(pd_params)
-    if personality_description.save
-    	flash[:notice] = "Saved"
-     	redirect_to '/personality_descriptions/new' 
-    else
-    	flash[:notice] = "Not Saved"
-     	redirect_to '/personality_descriptions/new'
-    end
-  end
 end
--- app/controllers/personality_dual_descriptions_controller.rb
+++ app/controllers/personality_dual_descriptions_controller.rb
@@ -1,18 +1,2 @@
 class PersonalityDualDescriptionsController < ApplicationController
-  def new
-  	personality_dual_description = PersonalityDualDescription.new
-  end
-
-  def create
-  	pdd_params = params.require(:personality_dual_description).permit(:category, :attribute_name, :high_description)
-  	puts pd_params
-  	personality_dual_description = PersonalityDualDescription.new(pdd_params)
-    if personality_dual_description.save
-    	flash[:notice] = "Saved"
-     	redirect_to '/personality_dual_descriptions/new' 
-    else
-    	flash[:notice] = "Not Saved"
-     	redirect_to '/personality_dual_descriptions/new'
-    end
-  end
 end
