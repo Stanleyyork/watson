8d4d1a7c256fbb55b7b1ccc80e7cf05472b87ef7
abeytaadam <measalion@gmail.com>
2016-01-09 18:43:22 -0800

    Fetch all tweets of user and save to db

--- lib/twitter_cred.rb
+++ lib/twitter_cred.rb
@@ -7,45 +7,42 @@ class TwitterInfo
       config.access_token = ENV['TWIT_ACCESS_TOKEN']
       config.access_token_secret = ENV['TWIT_ACCESS_TOKEN_SECRET']
     end
-    results = client.user_timeline(username, count: 200, include_rts: true)
+
+    # Recursively fetch all tweets
+    
+    def collect_with_max_id(collection=[], max_id=nil, &block)
+      response = yield(max_id)
+      collection += response
+      response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+    end
+
+    def client.get_all_tweets(user)
+      collect_with_max_id do |max_id|
+        options = {count: 200, include_rts: true}
+        options[:max_id] = max_id unless max_id.nil?
+        user_timeline(user, options)
+      end
+    end
+    
+    results = client.get_all_tweets(username)
     save_data(results, user_id)
   end
-  
-  # def self.client
-  #   puts 'client'
-  #   client ||= Twitter::REST::Client.new do |config|
-  #     config.consumer_key = ENV['TWIT_CONSUMER_KEY']
-  #     config.consumer_secret = ENV['TWIT_CONSUMER_SECRET']
-  #     config.access_token = ENV['TWIT_ACCESS_TOKEN']
-  #     config.access_token_secret = ENV['TWIT_ACCESS_TOKEN_SECRET']
-  #   end
-  # end
+
+  # Save each individual tweet to db
 
   def save_data(results, user_id)
     puts 'save_data'
-    puts results
     results.each do |tweet|
       puts tweet
-      puts '@savetweet = Channel.new'
-      puts '@savetweet.content = ' + tweet.text
-      puts '@savetweet.user_id = ' + user_id.to_s
-      puts '@savetweet.date = ' + tweet.created_at.to_s
-      puts '@savetweet.name = ' + "twitter"
-      puts '@savetweet.subname = ' + "account:" + tweet.user.screen_name
-      puts '@savetweet.year = ' + tweet.created_at.year.to_s
-      puts '@savetweet.num_entries = ' + 1
-      # @savetweet.save
-
-
-      # @savetweet = Channel.new
-      # @savetweet.content = tweet.text
-      # @savetweet.user_id = user_id
-      # @savetweet.date = tweet.
-      # @savetweet.name = "Twitter"
-      # @savetweet.subname = "account: #{username}"
-      # @savetweet.year = tweet.
-      # @savetweet.num_entries = 1
-      # @savetweet.save 
+      @savetweet = Channel.new
+      @savetweet.content = tweet.text 
+      @savetweet.user_id = user_id
+      @savetweet.date = tweet.created_at
+      @savetweet.name = "twitter"
+      @savetweet.subname = "account: " + tweet.user.screen_name
+      @savetweet.year = tweet.created_at.year
+      @savetweet.num_entries = 1
+      @savetweet.save
     end
   end
 end
