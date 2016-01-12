cc470e167b35878d761413aa4db6fabf797baa5d
stanleyyork <stanleyyork@gmail.com>
2016-01-11 17:10:43 -0800

    removed uid for user attri

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -35,8 +35,6 @@ class SessionsController < ApplicationController
       auth = env["omniauth.auth"]
       User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         @user.provider = auth.provider
-        @user.uid = auth.uid
-        @user.name = auth.info.name
         @user.facebook_access_token = auth.credentials.token
         @user.oauth_expires_at = Time.at(auth.credentials.expires_at)
         @user.save
